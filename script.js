eb3ec2ae46e8747315d839e66ca3caac3a04363f
alvslg <124530062+alvslg@users.noreply.github.com>
2023-11-06 23:26:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+alert('Bienvenu dans notre test de temps de réaction');
+
+let timer;
+let isRunning = false;
+let milliseconds = 0;
+let seconds = 0;
+let minutes = 0;
+
+function startStop() { // Cette fonction permet de démarrer et de faire le test
+    if (isRunning) { // Si le bouton Démarrer a été appuyé
+        clearInterval(timer);
+        document.getElementById("startStopButton").textContent = "Démarrer"; 
+        document.getElementById("startStopButton").classList.remove("running"); // Enlève la couleur blanche
+        document.getElementById("startStopButton").classList.add("stopped"); // La remplace par la couleure rouge
+        document.getElementById("chronometer").classList.remove("black-text"); //
+        document.getElementById("chronometer").classList.add("white-text"); // Cache le chronomètre
+    } else { // Si le bouton Arrêter est appuyé
+        document.getElementById("startStopButton").classList.remove("stopped");
+        document.getElementById("startStopButton").classList.add("running");
+        document.getElementById("chronometer").classList.remove("white-text");
+        document.getElementById("chronometer").classList.add("black-text");
+        timer = setInterval(updateChronometer, 10);
+        document.getElementById("startStopButton").textContent = "Arrêter";
+
+        // Défini un temps aléatoire entre 1 et 5 secondes avant de passer au vert
+        const randomDelay = Math.floor(Math.random() * 5000) + 1000; // Entre 1 et 5 secondes
+        setTimeout(() => {
+            document.getElementById("startStopButton").classList.remove("running");
+            document.getElementById("startStopButton").classList.add("stopped");
+            document.getElementById("chronometer").classList.add("visible");
+            document.getElementById("chronometer").classList.remove("black-text");
+            document.getElementById("chronometer").classList.add("white-text");
+            resetChronometer();
+            startStopAuto();
+        }, randomDelay);
+    }
+    isRunning = !isRunning;
+}
+
+
+function updateChronometer() {
+    milliseconds += 10;
+    if (milliseconds === 1000) {
+        milliseconds = 0;
+        seconds++;
+    }
+    if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+    }
+    document.getElementById("chronometer").textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}:${milliseconds < 10 ? '00' : milliseconds < 100 ? '0' : ''}${milliseconds}`;
+}
+
+function resetChronometer() {
+    clearInterval(timer);
+    document.getElementById("chronometer").textContent = '0:00:000';
+    milliseconds = 0;
+    seconds = 0;
+    minutes = 0;
+    isRunning = false;
+    document.getElementById("startStopButton").textContent = "Démarrer";
+}
+
+function startStopAuto() {
+    if (isRunning) {
+        clearInterval(timer);
+        document.getElementById("startStopButton").textContent = "Démarrer";
+        document.getElementById("startStopButton").classList.remove("running");
+        document.getElementById("startStopButton").classList.add("stopped");
+    } else {
+        document.getElementById("startStopButton").textContent = "Arrêter";
+        document.getElementById("startStopButton").classList.remove("stopped");
+        document.getElementById("startStopButton").classList.add("stopped");
+        timer = setInterval(updateChronometer, 10);
+    }
+    isRunning = !isRunning;
+}
+
+document.getElementById("startStopButton").addEventListener("click", startStop);
+document.getElementById("resetButton").addEventListener("click", resetChronometer);
+
